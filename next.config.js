167720e382226d0e6f60ac4da282733f055afdf4
Nada Hanad <jn_hanad@esi.dz>
2022-10-28 22:23:45 +0100

    styles

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["upload.wikimedia.org"],
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
